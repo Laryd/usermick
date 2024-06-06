d22350e3bf6af665232c2c22b1513393fddf9881
Laryd <hillarymuboka@gmail.com>
2024-06-06 12:24:27 -0700

    feat: add table to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { NavBar } from "./components/NavBar";
+import UsersTable from "./components/UsersTable";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <h1>home</h1>
+      <UsersTable/>
     </div>
   );
 }
